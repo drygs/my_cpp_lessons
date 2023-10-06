683553adc24923f6e215ecb28dc49ce3c285e292
DUZZlk <89948048+drygs@users.noreply.github.com>
2023-10-06 09:43:15 +0000

    Add files via upload

---
+++ teste 2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int  o_meu_numero_ = 10 ;                                      // isto é um comentario
+double O_meu_numero_real = 10.5;                               // números reais
+char o_meu_caracter = "a";                                     // caracteres
+string o_meu_texto = "isto é uma string";
+
+char teste = '10';
+
+int main()
+{
+    cout << "hello \\ World!\t";
+    cout << "I'm a C++ program\n";
+    cout << o_meu_numero_;
+    cout << "\n";
+    cout << o_meu_caracter + o_meu_caracter;
+    return 0;
+}
+int  o_meu_numero_ = 10; // isto é um comentario
