029ba63da74cdb4ac2ee9f4a878bf8d04b3f1d49
DUZZlk <89948048+drygs@users.noreply.github.com>
2023-10-06 10:30:57 +0000

    Add files via upload

--- teste 2.cpp
+++ teste 2.cpp
@@ -1,20 +1,34 @@
 #include <iostream>
 using namespace std;
 
-int  o_meu_numero_ = 10 ;                                      // isto é um comentario
+int  O_MEU_NUMERO = 10;                                        // isto é um comentario
 double O_meu_numero_real = 10.5;                               // números reais
-char o_meu_caracter = "a";                                     // caracteres
-string o_meu_texto = "isto é uma string";
+char o_meu_caracter = 'a';                                     // caracteres
+string o_meu_texto = "isto é uma string";                      // texto
+bool O_MEU_BOLEAN0 = true;                                     //booleano
+int x = 2, y = 3, z= 14;
 
-char teste = '10';
+int minutosPorHoras = 60;
+
+int numero;
+
+int main()
+{
+    cout << "digite um número: "; // imprime no ecrã
+    cin >> numero;// lê o teclado
+    cout << "O numero que digitaste foi: " << numero << endl       
+    return 0;
+}
+                                                                //tipo de variável nome da variavel = valor da varialvel
 
 int main()
 {
     cout << "hello \\ World!\t";
     cout << "I'm a C++ program\n";
-    cout << o_meu_numero_;
+    cout << O_MEU_NUMERO;
     cout << "\n";
     cout << o_meu_caracter + o_meu_caracter;
     return 0;
 }
-int  o_meu_numero_ = 10; // isto é um comentario
+
+int O_MEU_NUMERO = 10;                                         // isto é um comentario
