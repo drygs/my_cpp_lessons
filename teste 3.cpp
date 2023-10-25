228d017b677f93c8789b6b5076df5dfbfbc69b59
DUZZlk <89948048+drygs@users.noreply.github.com>
2023-10-25 11:38:33 +0000

    Add files via upload

---
+++ teste 3.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int a = 10;
+int b = 20;
+int c = 30;
+
+
+int main()
+{
+    if (a < b)
+    {
+        cout << "é vdd" << endl;
+    }
+    else if (a<c)
+    {
+        cout << " é vdd" << endl;
+    }
+    else if (a > c)
+    {
+        cout << "é mentira" << endl;
+    }
+
+    
+    
+}
---
+++ teste2.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+using namespace std;
+
+int O_MEU_NUMERO = 10;                  //Número Inteiro
+double O_MEU_NUMERO_REAL = 10.5;        //Número reais
+char O_MEU_CARACTER = 'a';              //Caracteres
+string O_MEU_TEXTO = "isto é um texto"; //Texto
+bool O_MEU_BOOLEANO = true;             //Booleanto
+const double PI = 3.1415;               //Constante
+int x = 2, y=3, z=14;
+int minutosPorHora=60;
+int numero;
+int raio;
+int main()
+
+{
+    //tendo em consideração que a área do circulo
+    //é PI * raio * raio, calcular a área do circulo
+    // com raio 10
+
+    //double c = PI;
+    //double d = 10;
+    //double e = 10;
+    //cout << c*d*e << endl;
+
+    //OU 
+
+    //int r = 8;
+    //double area = PI * r * r;
+    //cout << area << endl;
+
+    //cout <<" valor do raio: ";
+    //cin >> raio;
+    //double area = PI * raio * raio;
+    //cout << area << endl;
+
+
+    //int a = 1;
+    //int b = 2;
+    //cout << a+b << endl;
+
+    //cout << "Digita um numero: ";       //Imprime no ecrã
+    //cin >> numero;                      // Lê do teclado
+    //cout << "O numero que digistaste foi: " << numero << endl;
+
+    int a = 7;
+    int b = 5;
+    int c = 2;
+    int d = 1;
+    int resultado = a=2;
+    cout << resultado << endl;
+    // resto da divisão usa-se %
+    // ++ adiciona um valor à varial
+    // += ou -= é como se fosse a + 6 = 10 mas fica a+=6
+    //& serve para comparar 2 numeros
+
+    return 0;
+}
+
+
+int a = 10;
+int b = 20;
+int c = 30;
+
+
+int main()
+{
+    if (a < b)
+    {
+        cout << "é vdd" << endl;
+    }
+    else if (a<c)
+    {
+        cout << " é vdd" << endl;
+    }
+    else if (a > c)
+    {
+        cout << "é mentira" << endl;
+    }
+
+    
+    
+}
