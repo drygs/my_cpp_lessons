defd2f90e6b2187001396008702aca440c87f050
DUZZlk <ddssdiogo5@gmail.com>
2023-11-03 15:39:25 -0100

    DiogoSoares

--- chave_Da_Sorte.cpp
+++ chave_Da_Sorte.cpp
@@ -1,2 +1,20 @@
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
 using namespace std;
+
+int main()
+{
+    int num = 0;
+        srand((unsigned) time(0));
+        int numSorte = 1+(rand()%100);
+
+
+    int bolas[] = {num};
+    
+    for (1 i = 0; ; i++)
+    {
+        /* code */
+    }
+    
+}
