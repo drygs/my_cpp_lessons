1990cc3d6d2de55b3c91530660ee80ce2b5c0739
drygs <ddssdiogo5@gmail.com>
2024-01-30 10:38:32 -0100

    Diogo Soares

---
+++ exercicio2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+
+
+/*
+crie um programa capaz de multiplicar uma matriz bidimensional de inteirtos
+por um dado numero e criar uma nova matriz com o produto
+*/
+
+
+int arr[3][3] = { {1, 2, 3},
+                  {4, 5, 6},
+                  {7, 8, 9}};
+
+
+int arr[3][3] = {};
+
+int numeroMult;
+
+int main(){
+
+    cout << 'qual o numero para a multiplicação' << endl;
+    cin  >> numeroMult;
+
+
+}
+
+
