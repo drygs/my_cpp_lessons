2284d6f97deb8bb2429df92b04e2e0688ef39806
drygs <ddssdiogo5@gmail.com>
2024-02-06 10:39:14 -0100

    Diogo Soares

---
+++ 2module/a.py
@@ -0,0 +1,6 @@
+name = input("Enter your Family name: ")
+age = int(input("Enter your age: "))  # Cast the input to int
+
+print("Mr. {}, {} years old.".format(name, age))
+
+
---
+++ 2module/fncprintf.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+
+
+
+/*
+int main()
+{
+printf("caracteres:  %c %c %c \n ", 'a','b','c' );
+printf("Decimals : %d %ld\n", 1977, 650000L);
+printf("preceding with blanks: %10d, \n", 19778111);
+printf("Width trick: %*d \n", 10, 10);
+
+
+}
+*/
+
+
+/*int main()
+{
+    system("clear");
+    char nome[50];
+
+    printf("Nome: ");
+    system("clear");
+    scanf("%s", nome);
+    printf("ola %s \n ", nome);
+}*/
+
+string str; 
+int i;
+
+int main(){
+    system("clear");
+    printf("Enter your Family name: ");
+    scanf ("%s", &str);
+    printf("Enter your age: ");
+    scanf("%d", &i);
+    printf("Mr. %s, %d years old. \n", &str, i);
+    return 0;
+}
