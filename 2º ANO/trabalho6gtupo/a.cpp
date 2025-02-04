ba2ad812482e2a3e91bf39719a3db0abf36e8a1d
drygs <ddssdiogo5@gmail.com>
2025-02-04 13:02:24 -0100

    bubu balu

--- 2º ANO/trabalho6gtupo/a.cpp
+++ 2º ANO/trabalho6gtupo/a.cpp
@@ -18,10 +18,10 @@ string maiusculas() {
 }
 
 void exibirFrase(const string& frase, string& reveladas) {
-    // Exibe a frase com _ para as letras não reveladas
+    // Exibe a frase com _ para as letras não reveladas (ignora os espaços)
     for (int i = 0; i < frase.length(); i++) {
-        if (frase[i] == ' ' || reveladas[i] == ' ') {
-            cout << frase[i] << " ";  // Exibe a letra ou o espaço
+        if (frase[i] == ' ' || reveladas[i] == '1') {
+            cout << frase[i] << " ";  // Exibe a letra ou espaço
         } else {
             cout << "_ ";  // Exibe _ para as letras não reveladas
         }
@@ -29,22 +29,49 @@ void exibirFrase(const string& frase, string& reveladas) {
 
     cout << endl;
 
-    // Permite ao usuário tentar adivinhar uma letra
+    // Solicita ao usuário que adivinhe uma letra
     char letra;
     cout << "Digite uma letra: ";
     cin >> letra;
-    letra = toupper(letra);  // Converte a letra para maiúscula
+    letra = toupper(letra);  // Garante que a letra seja maiúscula
+
+    // Revela as letras na string reveladas se o usuário acertar
+    bool acertou = false;
+    for (int i = 0; i < frase.length(); i++) {
+        if (frase[i] == letra) {
+            reveladas[i] = '1';  // Revela a letra
+            acertou = true;
+        }
+    }
+
+    if (acertou) {
+        cout << "Você acertou! A letra '" << letra << "' foi revelada." << endl;
+    } else {
+        cout << "A letra '" << letra << "' não está na frase." << endl;
+    }
+}
+
+bool fraseRevelada(const string& reveladas, const string& frase) {
+    // Verifica se todas as letras foram reveladas, ignorando espaços
+    for (int i = 0; i < reveladas.length(); i++) {
+        if (frase[i] != ' ' && reveladas[i] == '0') { 
+            return false;  // Se encontrar um '0' e não for um espaço, significa que ainda há letras ocultas
+        }
+    }
+    return true;  // Se não houver '0', todas as letras foram reveladas
 }
 
 int main() {
-    string frase = maiusculas();  // Converte a frase para maiúsculas
-    string reveladas(frase.length(), '0');  // Inicializa todas as letras como não reveladas
+    string frase = maiusculas();
+    string reveladas(frase.length(), '0');  // Inicializa a string de revelações com '0'
 
-    // Repete a exibição até que todas as letras sejam reveladas
-    while (reveladas != string(frase.length(), '1')) {
-        exibirFrase(frase, reveladas);  // Exibe a frase e pede a adivinhação do usuário
+    // O loop continua enquanto a frase não estiver toda revelada
+    while (!fraseRevelada(reveladas, frase)) {
+        system("clear||cls");
+        exibirFrase(frase, reveladas);  
     }
 
+    cout << frase << endl;
     cout << "Parabéns! Você revelou toda a frase!" << endl;
 
     return 0;
