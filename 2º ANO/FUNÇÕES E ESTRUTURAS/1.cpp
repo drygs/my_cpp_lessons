24891dff98a531c29a6868853819e5138b467d07
drygs <ddssdiogo5@gmail.com>
2025-02-14 11:49:33 -0100

    aa

---
+++ 2º ANO/FUNÇÕES E ESTRUTURAS/1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+   int i = 0;
+   while(i < 100){
+    i++;
+    cout << i << endl;
+   }
+
+
+    switch (i){
+    case 1:
+        break;
+    case 2:
+        break;
+    case 3:
+        /* code */
+        break;
+    
+    default:
+        break;
+    }
+
+
+
+    int a = 2;
+    int b = 5;
+
+    if(a >= 5 && b >=5){
+        cout << "2" << endl;
+    }
+}
